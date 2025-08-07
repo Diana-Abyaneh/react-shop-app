e9aa5e20fc347a5a1fedd49203b6abeb507910d0
Diana Abyaneh <diana.abyaneh@gmail.com>
2025-08-07 20:55:46 +0330

    Enhance ProductsPage layout with product details and styling

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,4 +1,5 @@
 import useProducts from "../utils/useProducts.js";
+import styles from "./ProductPage.module.css"
 
 function ProductsPage() {
   const products = useProducts();
@@ -6,7 +7,11 @@ function ProductsPage() {
     <div>
       <ul>
         {products && products.map((product, id) => (
-          <li key={id}>{product.title}</li>
+          <li key={id}>
+            <img src={product.image} alt="product's image" />
+            <h3>{product.title}</h3>
+            <p>{product.price}$</p>
+          </li>
         ))}
       </ul>
     </div>
