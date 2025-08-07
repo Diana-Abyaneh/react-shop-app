2e19d68fb4219b11bb10fadfc74ffa1faa5b0019
Diana Abyaneh <diana.abyaneh@gmail.com>
2025-08-07 23:54:24 +0330

    Refactor ProductCard component and update CSS styles for improved layout and design

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,27 @@
-import styles from "./ProductCard.module.css"
+import styles from "./ProductCard.module.css";
+import { FaCartPlus } from "react-icons/fa";
+import { TbListDetails } from "react-icons/tb";
+import { Link } from "react-router-dom";
 
-function Cards({ data }) {
+function ProductCard({ data }) {
+  const { id, title, image, price } = data;
   return (
     <li className={styles.card}>
-      <img src={data.image} alt="product's image" />
-      <h4>{data.title}</h4>
-      <p>{data.price}$</p>
+      <img src={image} alt="product's image" />
+      <h4>{title}</h4>
+      <p>{price}$</p>
+      <div className={styles.actions}>
+        <Link to={`/products/${id}`}>
+          <TbListDetails />
+        </Link>
+        <div>
+          <button>
+            <FaCartPlus />
+          </button>
+        </div>
+      </div>
     </li>
   );
 }
 
-export default Cards;
+export default ProductCard;
