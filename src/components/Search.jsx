58570b5e12a2a1a9518510bcd2ba1b7671358cd7
Diana Abyaneh <diana.abyaneh@gmail.com>
2025-08-08 14:37:27 +0330

    Refactor ProductCard styles for consistency; add Search component styles and integrate into layout

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,17 @@
 import { IoSearch } from "react-icons/io5";
 import { useState } from "react";
+import styles from "./Search.module.css"
 
 function Search() {
+
   const [search, setSearch] = useState("");
+
   const searchHandler = () => {
     console.log(search);
   };
 
   return (
-    <div>
+    <div className={styles.searchContainer}>
       <input
         type="text"
         placeholder="Search..."
