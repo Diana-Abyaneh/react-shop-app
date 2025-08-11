e1bcd4f42490600e2db67cfd5179fb3a328fc2ce
Diana Abyaneh <diana.abyaneh@gmail.com>
2025-08-11 17:45:17 +0330

    Refactor CartContext reducer to improve item management logic; ensure state updates correctly for REMOVE, INCREASE, DECREASE, and CHECKOUT actions

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -13,6 +13,44 @@ const reducer = (state, action) => {
         ...sumProducts(state.selectedItems),
         checkout: false,
       };
+    case "REMOVE_ITEM": {
+      const newSelectedItems = state.selectedItems.filter(
+        (item) => item.id != action.payload.id
+      );
+      return {
+        ...state,
+        selectedItems: [...newSelectedItems],
+        ...sumProducts(newSelectedItems),
+      };
+    }
+    case "INCREASE": {
+      const index = state.selectedItems.findIndex(
+        (item) => item.id == action.payload.id
+      );
+      state.selectedItems[index].quantity++;
+      return {
+        ...state,
+        ...sumProducts(state.selectedItems),
+      };
+    }
+    case "DECREASE":
+      {
+        const index = state.selectedItems.findIndex(
+          (item) => item.id == action.payload.id
+        );
+        state.selectedItems[index].quantity--;
+      }
+      return {
+        ...state,
+        ...sumProducts(state.selectedItems),
+      };
+    case "CHECKOUT":
+      return {
+        selectedItems: [],
+        itemsCounter: 0,
+        total: 0,
+        checkout: true,
+      };
     default:
       throw new Error("Invalid action!");
   }
