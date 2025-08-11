ffcb4d950d6e270acc68d21f8f1cf537d704e2f2
Diana Abyaneh <diana.abyaneh@gmail.com>
2025-08-11 19:01:46 +0330

    Enhance ProductCard functionality with quantity management; implement ADD, REMOVE, INCREASE, and DECREASE actions for cart items

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,17 +1,24 @@
 import styles from "./ProductCard.module.css";
 import { FaCartPlus } from "react-icons/fa";
 import { TbListDetails } from "react-icons/tb";
+import { FaPlus } from "react-icons/fa6";
+import { FaMinus } from "react-icons/fa6";
+import { FaTrash } from "react-icons/fa";
+
 import { Link } from "react-router-dom";
 import useCart from "../utils/useCart";
+import { productQuantity } from "../helper/helper";
 
 function ProductCard({ data }) {
   const { id, title, image, price } = data;
 
   const [state, dispatch] = useCart();
-  console.log(state);
-  
-  const clickHandler = () => {
-    dispatch({ type: "ADD_ITEM", payload: data });
+  // console.log(state);
+  const quantity = productQuantity(state, id);
+  console.log(quantity);
+
+  const clickHandler = (type) => {
+    dispatch({ type, payload: data });
   };
 
   return (
@@ -24,9 +31,33 @@ function ProductCard({ data }) {
           <TbListDetails />
         </Link>
         <div>
-          <button onClick={clickHandler}>
-            <FaCartPlus />
-          </button>
+          {quantity == 1 && (
+            <>
+              <button onClick={() => clickHandler("REMOVE_ITEM")}>
+                <FaTrash />
+              </button>
+              <span>{quantity}</span>
+            </>
+          )}
+
+          {quantity > 1 && (
+            <>
+              <button onClick={() => clickHandler("DECREASE")}>
+                <FaMinus />
+              </button>
+              <span>{quantity}</span>
+            </>
+          )}
+          
+          {quantity == 0 ? (
+            <button onClick={() => clickHandler("ADD_ITEM")}>
+              <FaCartPlus />
+            </button>
+          ) : (
+            <button onClick={() => clickHandler("INCREASE")}>
+              <FaPlus />
+            </button>
+          )}
         </div>
       </div>
     </li>
