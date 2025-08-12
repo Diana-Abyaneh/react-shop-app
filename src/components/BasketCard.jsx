076c4a136cfedabdb6ccd9f56eefe85188401496
Diana Abyaneh <diana.abyaneh@gmail.com>
2025-08-12 22:31:13 +0330

    Refactor BasketCard and CheckoutPage components; add CSS modules for improved styling and layout

--- src/components/BasketCard.jsx
+++ src/components/BasketCard.jsx
@@ -1,15 +1,15 @@
 import { FaPlus } from "react-icons/fa6";
 import { FaMinus } from "react-icons/fa6";
 import { FaTrash } from "react-icons/fa";
-
+import styles from "./BasketCard.module.css"
 function BasketCard({ data, clickHandler }) {
   const { image, title, quantity } = data;
 
   return (
-    <div>
+    <div className={styles.container}>
       <img src={image} alt={`${title} image`} />
       <h4>{title}</h4>
-      <div>
+      <div className={styles.buttonsContainer}>
         {quantity == 1 ? (
           <button onClick={() => clickHandler("REMOVE_ITEM", data)}>
             <FaTrash />
--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,5 +1,9 @@
 import BasketCard from "../components/BasketCard.jsx";
 import useCart from "../utils/useCart.js";
+import styles from "./CheckoutPage.module.css";
+import { FaMoneyCheckAlt } from "react-icons/fa";
+import { MdNumbers } from "react-icons/md";
+import { LuBadgeCheck } from "react-icons/lu";
 
 function CheckoutPage() {
   const [state, dispatch] = useCart();
@@ -9,22 +13,29 @@ function CheckoutPage() {
   };
 
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.details}>
         <span>
+          <FaMoneyCheckAlt />
           <p>Total: {state.total}</p>
         </span>
         <span>
+          <MdNumbers />
           <p>Quantity: {state.itemsCounter}</p>
         </span>
         <span>
+          <LuBadgeCheck />
           <p>Status: {state.checkout ? "Done!" : "Pending..."}</p>
         </span>
         <button>Checkout</button>
       </div>
-      <div>
+      <div className={styles.products}>
         {state.selectedItems.map((product) => (
-          <BasketCard key={product.id} data={product} clickHandler={clickHandler}/>
+          <BasketCard
+            key={product.id}
+            data={product}
+            clickHandler={clickHandler}
+          />
         ))}
       </div>
     </div>
